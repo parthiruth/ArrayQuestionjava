d18e011a274be775080a0b4a3b4104f875a2bec3
parthiruth <31817166+parthiruth@users.noreply.github.com>
2018-01-03 13:49:38 +0530

    Add files via upload

---
+++ equalize the array.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class equalize the array {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[100+1];
+        for(int i=0;i<n;i++)
+            //a[i]=sc.nextInt();
+        {
+           a[sc.nextInt()]++;
+        }
+        int max=0,sum=0;
+          for(int i=0;i<=100;i++)
+              {
+                 if(a[i] > max)
+                     {
+                      max=a[i];
+                      }
+                 sum=a[i]+sum;
+          }
+        System.out.print(Math.abs(sum-max));
+               
+                 
+          
+        
+           
+                    
+                            
+        
+    }
+}
